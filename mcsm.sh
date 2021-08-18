5b7f009528ef121ea2d3cfc7585e71b52bdf4233
efsg <39408740+efsg@users.noreply.github.com>
2021-08-18 17:40:07 +0800

    Update mcsm.sh
    添加自动更新

--- mcsm.sh
+++ mcsm.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-if [ ! -f "/opt/mcsm/app.js" ]; then
-    git clone https://gitee.com/Suwingser/MCSManager /opt/mcsm
-    npm install --production
-fi
+set -eu
+	if [ ! -f "/opt/mcsm/app.js" ]; then
+		echo "未找到面板，开始安装。"
+		git clone https://gitee.com/Suwingser/MCSManager /opt/mcsm
+		npm install --production
+	fi
+
+	if [ "$update" == "true"  ]; then
+		echo "更新面板。"
+		git pull
+	else
+		echo "跳过更新。"
+	fi
+echo "启动面板。"
 node app.js
